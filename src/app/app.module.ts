b0ba1f0e892afec8a8a91f0fa97535b6314d8e96
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2025-01-28 14:38:16 +0100

    affichage date en français

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { RouteReuseStrategy } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { LOCALE_ID } from '@angular/core';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient()],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: LOCALE_ID, useValue: 'fr-FR' }, provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
