d2a3a2eadb633ad691757cd54ba46cfeed36ac56
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2025-01-28 15:08:54 +0100

    search bar

--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule, ModalController} from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { TaskService } from '../../services/task.service';
 import { TaskFormComponent } from '../task-form/task-form.component';
 import { Task } from '../../../models/task.model';
-import {DatePipe, NgForOf, NgIf} from "@angular/common";
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  imports: [
-    IonicModule,
-    NgForOf,
-    DatePipe,
-    NgIf
-  ],
-  styleUrls: ['./task-list.component.scss']
+  imports: [IonicModule, NgForOf, DatePipe, NgIf],
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
+  filteredTasks: Task[] = [];
 
   constructor(
     private modalController: ModalController,
@@ -30,6 +26,7 @@ export class TaskListComponent implements OnInit {
 
   loadTasks(): void {
     this.tasks = this.taskService.getTasks();
+    this.filteredTasks = [...this.tasks]; // Initialiser les tâches filtrées
   }
 
   async openTaskForm() {
@@ -37,7 +34,7 @@ export class TaskListComponent implements OnInit {
       component: TaskFormComponent,
       breakpoints: [0, 0.5, 1],
       initialBreakpoint: 0.5,
-      handleBehavior: 'cycle'
+      handleBehavior: 'cycle',
     });
 
     await modal.present();
@@ -57,5 +54,17 @@ export class TaskListComponent implements OnInit {
   deleteTask(index: number): void {
     this.tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    this.filteredTasks = [...this.tasks]; // Mettre à jour les tâches filtrées
+  }
+
+  filterTasks(event: any): void {
+    const searchTerm = event.target.value.toLowerCase();
+    if (searchTerm && searchTerm.trim() !== '') {
+      this.filteredTasks = this.tasks.filter((task) =>
+        task.name.toLowerCase().includes(searchTerm)
+      );
+    } else {
+      this.filteredTasks = [...this.tasks];
+    }
   }
 }
